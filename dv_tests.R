76f0a6fae1f0b08d903bea16cd8a15f897377477
matthew-a-dunlap <matthew_dunlap@harvard.edu>
2023-03-08 19:23:45 -0500

    Pass test logic function as pointer to mainpath wrapper
    This allows us to write our mainpath logic once, not in each test

--- dv_tests.R
+++ dv_tests.R
@@ -4,36 +4,47 @@ library(askpass)
 source('dv_api.R')
 
 mainpath_state <- TRUE
+wait_offset <- 0 #If a target system is running slow, bump this to increase the wait globally
 
 # TODO:
 # - Begin implementing R03-R25
 
-r01alt_mainpath_builtin_auth <- function() {
+# Wrapper for all mainpath test functions. Allows us to not rewrite the same code over-and-over for all tests
+call_mainpath <- function(FUN) {
   if(!mainpath_state) { return(mainpath_state) } #Fails if previous mainpath tests have failed
   
   mainpath_state <<- tryCatch({
-    username <- askpass("Username for builtin account")
-    password <- askpass("Password for builtin account")
-    #username <- ''
-    #password <- ''
-    
-    sesh$navigate(paste(dv_server_url,'/loginpage.xhtml?redirectPage=%2Fdataverse.xhtml', sep=''))
-    #Sys.sleep(1)
-    sesh$findElement(using="xpath", value='//*[@id="loginForm:credentialsContainer:0:credValue"]')$sendKeysToElement(list(username))
-    sesh$findElement(using="xpath", value='//*[@id="loginForm:credentialsContainer:1:sCredValue"]')$sendKeysToElement(list(password, key="enter"))
-    #NOTE: For some reason this button click works with directly setting username/password, but NOT when using askpass.
-    #      currently the code is just triggering an enter key after entering the password, but we may have the issue again
-    #sesh$findElement(using="xpath", value='//*[@id="loginForm:login"]')$clickElement()
-    
-    expect_identical(1, 2)
+    FUN()
     return(TRUE)
   }, expectation_failure=function(e) { #catch testthat failures
+    print(e)
     return(FALSE)
   },error = function(e) { #catch general errors
+    print(e)
     return(false)
   })
-    
+  
   return(mainpath_state)
+}
+
+r01alt_mainpath_builtin_auth <- function() {
+  username <- askpass("Username for builtin account")
+  password <- askpass("Password for builtin account")
+  #username <- ''
+  #password <- ''
+  
+  sesh$navigate(paste(dv_server_url,'/loginpage.xhtml?redirectPage=%2Fdataverse.xhtml', sep=''))
+  #
+  sesh$findElement(using="xpath", value='//*[@id="loginForm:credentialsContainer:0:credValue"]')$sendKeysToElement(list(username))
+  sesh$findElement(using="xpath", value='//*[@id="loginForm:credentialsContainer:1:sCredValue"]')$sendKeysToElement(list(password, key="enter"))
+  #NOTE: For some reason this button click works with directly setting username/password, but NOT when using askpass.
+  #      currently the code is just triggering an enter key after entering the password, but we may have the issue again
+  #sesh$findElement(using="xpath", value='//*[@id="loginForm:login"]')$clickElement()
+  #sesh$screenshot(file = 'atest.png')
+  
+  Sys.sleep(1 + wait_offset) #Wait for page load. We should be able to use setImplicitWaitTimeout, but that doesn't seem to work currently due to rselenium being stale
+  
+  expect_identical(paste(dv_server_url,'/dataverse.xhtml', sep=''), toString(sesh$getCurrentUrl()))
 }
 
 load_dataverse_admin_info_from_file <- function() {
@@ -53,13 +64,12 @@ begin_user_browser <- function() {
     port = 4444L,
     browserName = "safari"
   )
-  sesh$errorDetails()
+  #sesh$errorDetails()
   sesh$open()
-  sesh$getStatus()
+  #sesh$getStatus()
 }
 
 load_dataverse_admin_info_from_file()
 begin_user_browser()
-r01alt_mainpath_builtin_auth()
-
+call_mainpath(r01alt_mainpath_builtin_auth)
 sesh$closeWindow()
